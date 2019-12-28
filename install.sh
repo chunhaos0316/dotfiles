6cc7c068348150474f0a3dcc5418655ee9a7be0b
haodance <cax743100@gmail.com>
2019-12-28 18:13:07 +0800

    Add homebrew & amend install.sh structure to loop
    [homebrew]
    1.add homebrew installation
    
    [vim]
    1.Add snipmate
    2.Add vundle installation
    
    [install.sh]
    1.Convert to loop

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+installHomebrew(){
+    echo Installing \"Homebrew\" ...
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
 setGit(){
     echo Setting \".gitconfig\" ...
     ln -fs `pwd`/Git/gitconfig ~/.gitconfig
@@ -8,6 +13,17 @@ setGit(){
 setVim(){
     echo Setting \".vimrc\" ...
     ln -fs `pwd`/Vim/vimrc ~/.vimrc
+    echo
+
+    echo Installing vundle ...
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    vim +PluginInstall +qall
+
+    echo Setting \"snipmate\" ...
+    for snippet in $(ls `pwd`/SnipMate/)
+    do
+        ln -fs `pwd`/SnipMate/$snippet ~/.vim/snippets/$snippet
+    done
 }
 
 setTmux(){
@@ -20,38 +36,45 @@ setZsh(){
     ln -fs `pwd`/Zsh/zshrc ~/.zshrc
 }
 
-echo "Select the dotfiles to be set."
-echo "1) All (including git, vim, tmux, zsh)"
-echo "2) Git"
-echo "3) Vim"
-echo "4) Tmux"
-echo "5) Zsh"
-echo "(type q to quit)"
-echo
+until [ "$SETTING_TARGET" == "q" ]
+do
+    echo "Select the dotfiles to be set."
+    echo "1) All (including git, vim, tmux, zsh)"
+    echo "2) Git"
+    echo "3) Vim"
+    echo "4) Tmux"
+    echo "5) Zsh"
+    echo "6) Homebrew"
+    echo "(Enter or type q to quit)"
+    echo
 
-read -p "Setting target: " SETTING_TARGET
-SETTING_TARGET=${SETTING_TARGET:-"q"}
-echo
+    read -p "Setting target: " SETTING_TARGET
+    SETTING_TARGET=${SETTING_TARGET:-"q"}
+    echo
 
-case $SETTING_TARGET in
-    "1")
-        setGit; setVim; setTmux; setZsh
-        ;;
-    "2")
-        setGit
-        ;;
-    "3")
-        setVim
-        ;;
-    "4")
-        setTmux
-        ;;
-    "5")
-        setZsh
-        ;;
-    "q")
-        ;;
-    *)
-        echo "Invalid Operation!!"
-        ;;
-esac
+    case $SETTING_TARGET in
+        "1")
+            installHomebrew; setGit; setVim; setTmux; setZsh
+            ;;
+        "2")
+            setGit
+            ;;
+        "3")
+            setVim
+            ;;
+        "4")
+            setTmux
+            ;;
+        "5")
+            setZsh
+            ;;
+        "6")
+            installHomebrew
+            ;;
+        "q")
+            ;;
+        *)
+            echo "Invalid Operation!!"
+            ;;
+    esac
+done
